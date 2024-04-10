69eec1e260f653d32e3a743b1f8352b86b5ce1b6
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2024-04-10 08:36:34 +0700

    change app.js

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ var app = express();
 
 app.use(cors())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
